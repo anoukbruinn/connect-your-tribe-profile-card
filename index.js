9af7c15d7716fb56f93a7e4f79d4c68dfdfa10ef
anoukbruinn <112856687+anoukbruinn@users.noreply.github.com>
2023-02-08 11:59:32 +0100

    done

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+
 // Importeer express uit de node_modules map
-import express from 'express'
+import express, { response } from 'express'
+
+const url = 'https:/whois.fdnd.nl/api/v1/member/anouk'
+const data = await fetch(url).then((response) => response.json())
 
 // Maak een nieuwe express app aan
 const app = express()
